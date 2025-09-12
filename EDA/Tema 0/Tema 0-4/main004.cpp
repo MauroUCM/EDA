2d18323f62c7fd51207a50ef2354b9d8cf07f233
MauroUCM <maumar03@ucm.es>
2025-09-12 18:08:56 +0200

    Hasta el tema04

--- EDA/Tema 0/Tema 0-4/main004.cpp
+++ EDA/Tema 0/Tema 0-4/main004.cpp
@@ -1,4 +1,4 @@
-// Mauro Martinez Montes
+﻿// Mauro Martinez Montes
 // EDA-GDV46
 
 
@@ -7,45 +7,47 @@
 #include <fstream>
 
 
+
 // función que resuelve el problema
-TipoSolucion resolver(TipoDatos datos) {
-    
-    
+int resolver(int datos) {
+    return datos * 2;
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
     // leer los datos de la entrada
-    
-    if (caso especial)
+    int datos;
+    std::cin >> datos;
+
+
+    if (!std::cin)
         return false;
-    
-    TipoSolucion sol = resolver(datos);
-    
+
     // escribir sol
-    
+    std::cout << resolver(datos) << "\n";
+
     return true;
 }
 
 int main() {
     // Para la entrada por fichero.
     // Comentar para acepta el reto
-    #ifndef DOMJUDGE
-     std::ifstream in("Tema 0/Tema 0-3/1.in");
-     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-     #endif 
-    
-    
+#ifndef DOMJUDGE
+    std::ifstream in("Tema 0/Tema 0-4/1.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
     while (resuelveCaso())
         ;
 
-    
+
     // Para restablecer entrada. Comentar para acepta el reto
-     #ifndef DOMJUDGE // para dejar todo como estaba al principio
-     std::cin.rdbuf(cinbuf);
-     system("PAUSE");
-     #endif
-    
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
     return 0;
 }
