a2139acb3a9b0df887669061658a329f3eb77d4f
MauroUCM <maumar03@ucm.es>
2025-11-07 16:04:02 +0100

    setup 65

---
+++ EDA/Tema 6/Tema 6-5/main605.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include "bintree_eda.h"
+using namespace std;
+
+bool resuelve(bintree<int> tree) {
+
+    return true;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+
+    bintree<int> tree = leerArbol(int(-1));
+    
+    if (resuelve(tree)) cout << "SI" << endl;
+    else cout << "NO" << endl;
+
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("Tema 6/Tema 6-5/1.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+    int i;
+    cin >> i;
+
+    for (int u = 0; u < i; u++) resuelveCaso();
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    //system("PAUSE");
+#endif
+
+    return 0;
+}
